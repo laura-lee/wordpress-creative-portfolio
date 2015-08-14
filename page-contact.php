601807e0a314b11b81129df46d8b1bc4f1690f63
Lee <lee-macdonald@hotmail.com>
2015-08-14 19:52:58 -0400

    the wknd is upon us

--- page-contact.php
+++ page-contact.php
@@ -1,28 +1,34 @@
+<?php get_header();  ?>
 <div class="wrapper">
-  <?php
 
-  /*
-  	Template Name: Contact Page. 
-  */
+  	<!-- Template Name: Contact Page.  -->
 
-  get_header();  ?>
+  
 
   <div class="main">
-    <div class="container">
+    <div class="container contact-page">
 
 
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <h2><?php the_title(); ?></h2>
+        <?php  dynamic_sidebar( 'third-widget-area' ); ?>
+        <div class="social-media-widgets">
+    
+    <?php  dynamic_sidebar( 'primary-widget-area' ); ?>
+  </div>
         <?php the_content(); ?>
 
       <?php endwhile; // end the loop?>
     </div> <!-- /.container -->
 
-
+  <!-- <div class="social-media-widgets">
+    
+    <?php  dynamic_sidebar( 'primary-widget-area' ); ?>
+  </div> -->
 
   </div> <!-- /.main -->
 
-  <?php get_footer(); ?>
 </div>
+<?php get_footer(); ?>
